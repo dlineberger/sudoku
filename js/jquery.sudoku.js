ececeb7a6526137f31c29eac55bb1539b945b7a2
David Lineberger <dlineberger@gmail.com>
2014-08-11 19:10:26 -0500

    Removing Sudoku.js.

--- js/jquery.sudoku.js
+++ js/jquery.sudoku.js
@@ -23,7 +23,17 @@
 		return null;
 	}
 
-	var board = (restoreBoard() || new Sudoku());
+	var board = (restoreBoard() || [
+		[ {answer: 5, permanent: true}, {answer: 3, permanent: true}, {answer: 4}, {answer: 6}, {answer: 7, permanent: true}, {answer: 8}, {answer: 9}, {answer: 1}, {answer: 2} ],
+		[ {answer: 6, permanent: true}, {answer: 7}, {answer: 2}, {answer: 1, permanent: true}, {answer: 9, permanent: true}, {answer: 5, permanent: true}, {answer: 3}, {answer: 4}, {answer: 8} ],
+		[ {answer: 1}, {answer: 9, permanent: true}, {answer: 8, permanent: true}, {answer: 3}, {answer: 4}, {answer: 2}, {answer: 5}, {answer: 6, permanent: true}, {answer: 7} ],
+		[ {answer: 8, permanent: true}, {answer: 5}, {answer: 9}, {answer: 7}, {answer: 6, permanent: true}, {answer: 1}, {answer: 4}, {answer: 2}, {answer: 3, permanent: true} ],
+		[ {answer: 4, permanent: true}, {answer: 2}, {answer: 6}, {answer: 8, permanent: true}, {answer: 5}, {answer: 3, permanent: true}, {answer: 7}, {answer: 9}, {answer: 1, permanent: true} ],
+		[ {answer: 7, permanent: true}, {answer: 1}, {answer: 3}, {answer: 9}, {answer: 2, permanent: true}, {answer: 4}, {answer: 8}, {answer: 5}, {answer: 6, permanent: true} ],
+		[ {answer: 9}, {answer: 6, permanent: true}, {answer: 1}, {answer: 5}, {answer: 3}, {answer: 7}, {answer: 2, permanent: true}, {answer: 8, permanent: true}, {answer: 4} ],
+		[ {answer: 2}, {answer: 8},	{answer: 7}, {answer: 4, permanent: true}, {answer: 1, permanent: true}, {answer: 9, permanent: true}, {answer: 6}, {answer: 3}, {answer: 5, permanent: true}],
+		[ {answer: 3}, {answer: 4}, {answer: 5}, {answer: 2}, {answer: 8, permanent: true}, {answer: 6}, {answer: 1}, {answer: 7, permanent: true}, {answer: 9, permanent: true} ]
+	]);
 
 	// Construct the board
 	var drawBoard = function($element) {
